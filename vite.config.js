869198d4e56afc112d80a08035c7bae9572e0089
jo43w <yusif2006salah@gmail.com>
2026-09-09 03:55:59 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: './',
+  base: command === 'build' ? '/NESORA9/' : '/',
   server: { port: 5173 }
-})
+}))
